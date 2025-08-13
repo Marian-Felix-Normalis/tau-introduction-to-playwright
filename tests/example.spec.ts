0ad8572b8b1913898bdaf381d0c91879232fd821
Marian Felix Pletzer <mpletzer@normalis.de>
2025-08-13 16:31:30 +0200

    Add test from tut

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -31,6 +31,19 @@ test('get started link', async ({ page }) => {
  * 
  */
 
+test('myFromTutTest', async ({ page }) => {
+
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link', {name: 'Get started'}).click();
+  await page.getByRole('button', {name: ' Node.js'}).hover();
+  await page.getByText('Java', {exact:true}).click();
+  await expect(page).toHaveURL('https://playwright.dev/java/docs/intro')
+  await expect(page.getByText('Installing Playwright', {exact:true})).not.toBeVisible();
+  await expect(page.getByText('Playw1right is distributed as a set of Maven modules. The easiest way to use it is to add one dependency to your project\'s pom.xml as described below. If you\'re not familiar with Maven please refer to its documentation.')).toBeVisible();
+
+});
+
+
 // test.only('check Java page', async ({ page }) => {
 test('check Java page', async ({ page }) => {
 
